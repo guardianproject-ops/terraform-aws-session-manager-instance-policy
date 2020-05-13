0eeccc22d4e95fc0033d2f32fc861ec8a216cefa
Abel Luck <abel@guardianproject.info>
2020-05-13 14:20:46 +0200

    feat: Support 'stage' and 'environment' label params

--- main.tf
+++ main.tf
@@ -3,13 +3,14 @@ terraform {
 }
 
 module "label" {
-  source     = "git::https://github.com/cloudposse/terraform-null-label.git?ref=tags/0.16.0"
-  namespace  = var.namespace
-  stage      = var.stage
-  name       = var.name
-  delimiter  = var.delimiter
-  attributes = var.attributes
-  tags       = var.tags
+  source      = "git::https://github.com/cloudposse/terraform-null-label.git?ref=tags/0.16.0"
+  namespace   = var.namespace
+  stage       = var.stage
+  environment = var.environment
+  name        = var.name
+  delimiter   = var.delimiter
+  attributes  = var.attributes
+  tags        = var.tags
 }
 
 data "aws_caller_identity" "current" {}
