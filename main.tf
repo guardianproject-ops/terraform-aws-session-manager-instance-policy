02dbc1c3e6053072710a9a65fbbe686403702ed7
Abel Luck <abel@guardianproject.info>
2020-09-14 12:32:32 +0200

    feat: Update to context.tf

--- main.tf
+++ main.tf
@@ -2,17 +2,6 @@ terraform {
   required_version = ">= 0.12"
 }
 
-module "label" {
-  source      = "git::https://github.com/cloudposse/terraform-null-label.git?ref=tags/0.19.2"
-  namespace   = var.namespace
-  stage       = var.stage
-  environment = var.environment
-  name        = var.name
-  delimiter   = var.delimiter
-  attributes  = var.attributes
-  tags        = var.tags
-}
-
 data "aws_caller_identity" "current" {}
 
 data "aws_iam_policy_document" "session_manager_with_logs" {
@@ -71,7 +60,7 @@ data "aws_iam_policy_document" "session_manager_with_logs" {
 }
 
 resource "aws_iam_policy" "session_manager_with_logs" {
-  name        = "session-manager-with-logs-${module.label.id}"
+  name        = "session-manager-with-logs-${module.this.id}"
   description = "Policy that allows instances capability to be used by the session manager and log sessions to S3"
   policy      = data.aws_iam_policy_document.session_manager_with_logs.json
 }
